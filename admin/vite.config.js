295edb3989691970674885585a6de463beb31aff
bouricha walid <onepiece28102000@gmail.com>
2026-08-18 21:49:51 +0200

    probleme de recherche chunk

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,19 +3,31 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 3001,
-    watch: process.env.USE_POLLING === 'true'
-      ? { usePolling: true, interval: 300 }
-      : undefined,
+    watch:
+      process.env.USE_POLLING === "true"
+        ? { usePolling: true, interval: 300 }
+        : undefined,
   },
+
   build: {
     outDir: "dist",
+
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          router: ['react-router-dom'],
+        manualChunks(id) {
+          if (id.includes("node_modules/react-router-dom")) {
+            return "router";
+          }
+
+          if (
+            id.includes("node_modules/react") ||
+            id.includes("node_modules/react-dom")
+          ) {
+            return "vendor";
+          }
         },
       },
     },
